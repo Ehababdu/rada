7018112dcd4c958fa0ec949365076e2b155dd569
Ehab <ehab@example.com>
2026-02-04 01:58:04 +0200

    add

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::get('/', function () {
     ]);
 })->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
     Route::resource('martyrs', MartyrController::class);
